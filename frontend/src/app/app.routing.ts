0ede99a278a7373b2c5f1fa60e5861746065e78f
Valeria-Sem <lerasemenenya@gmail.com>
2020-03-19 14:16:41 +0300

    1st page

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {HomeComponent} from "./components/home/home.component";
+
+
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent}
+  // , {path: 'about', component: AboutComponent}
+  // , {path: 'userReg', component: UserRegComponent}
+  // , {path: 'orgReg', component: OrganisationRegComponent}
+  // , {path: 'films', component: FilmsComponent, canActivate: [RoleGuard], data: {role: Role.CUSTOMER}}
+  // , {path: 'app', component: AppsComponent, canActivate: [RoleGuard], data: {role: Role.CUSTOMER}}
+  // , {path: 'games', component: GamesComponent, canActivate: [RoleGuard], data: {role: Role.CUSTOMER}}
+  // , {path: 'music', component: MusicComponent, canActivate: [RoleGuard], data: {role: Role.CUSTOMER}}
+  // , {path: 'sites', component: SitesComponent, canActivate: [RoleGuard], data: {role: Role.CUSTOMER}}
+  // , {path: 'profile', component: UserComponent}
+  // , {path: 'sub', component: SubComponent}
+  // , {path: 'constructor', component: ConstructorComponent, canActivate: [RoleGuard], data: {role: Role.ORGANISATION}}
+  // , {path: '**', component: NotFoundComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
---
+++ frontend/src/app/components/nav-side/nav-side.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: "app-nav-side",
+  templateUrl: "./nav-side.component.html",
+  styleUrls: ['./nav-side.component.css']
+})
+export class NavSideComponent implements OnInit{
+  ngOnInit(): void {
+  }
+}
