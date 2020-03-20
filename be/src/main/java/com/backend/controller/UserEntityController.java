6c113da1681a450abb92e7dbb678bd8cebc85c74
Valeria-Sem <lerasemenenya@gmail.com>
2020-03-20 18:53:03 +0300

    created UserEntityController and tested it in the postman

---
+++ be/src/main/java/com/backend/controller/UserEntityController.java
@@ -0,0 +1,42 @@
+package com.backend.controller;
+
+import com.backend.entity.UserEntity;
+import com.backend.service.UserEntityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserEntityController {
+
+    @Autowired
+    private UserEntityService userEntityService;
+
+    @RequestMapping(value = "/login/{login}", method = RequestMethod.GET)
+    public ResponseEntity<UserEntity> getUsersByLogin(@PathVariable(name = "login") String login) {
+        UserEntity userEntity = userEntityService.findByLogin(login);
+        return ResponseEntity.ok(userEntity);
+    }
+    @RequestMapping(value = "/login/{login}/{pass}", method = RequestMethod.GET)
+    public ResponseEntity<UserEntity> getUsersByLoginP(@PathVariable(name = "login") String login,
+                                                        @PathVariable(name = "pass") String pass) {
+        UserEntity userEntity = userEntityService.getUserIdByLoginAndPassword(login, pass);
+        return ResponseEntity.ok(userEntity);
+    }
+
+    @RequestMapping(value = "/delete/{idUser}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable(name = "idUser") Integer idUser) {
+        userEntityService.delete(idUser);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<UserEntity> getAllUsers(){
+        return userEntityService.getAllUsers();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public UserEntity saveUser(@RequestBody UserEntity userEntity){
+        return userEntityService.save(userEntity);
+    }
+}
--- be/src/main/java/com/backend/service/impl/UserEntityServiceImpl.java
+++ be/src/main/java/com/backend/service/impl/UserEntityServiceImpl.java
@@ -4,7 +4,9 @@ import com.backend.entity.UserEntity;
 import com.backend.repository.UserEntityRepository;
 import com.backend.service.UserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserEntityServiceImpl implements UserEntityService {
 
     @Autowired
